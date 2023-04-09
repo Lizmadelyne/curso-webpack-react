92e41d7e92f4c3eb79419f349f3fe9f0f9e0acce
Lizmadelyne <lizfajardop@gmail.com>
2023-04-09 07:49:01 -0500

    :tada: initial proyect

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+
+    return(
+        <div>
+            <h1>Hello  i am liz  the best</h1>
+            <p>hola aqui todo fue dificil</p>
+            <ul>
+                <li>configurar</li>
+            </ul>
+
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactDOM } from "react";
+import  App from './components/App.jsx';
+import './styles/global.scss';
+
+ReactDOM.render(
+    <React.StrictMode>
+     <App />
+    </React.StrictMode>,
+    document.getElementById('app')
+    );
