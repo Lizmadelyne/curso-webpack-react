f63da213007a2f1aec0214ba6b11324c517e41dd
Lizmadelyne <lizfajardop@gmail.com>
2023-04-09 07:56:32 -0500

    little changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,8 @@ function App(){
 
     return(
         <div>
-            <h1>Hello  i am liz  the best</h1>
-            <p>hola aqui todo fue dificil</p>
-            <ul>
-                <li>configurar</li>
-            </ul>
+            <h1>Hello</h1>
+            <p>webpack is hard</p>
 
         </div>
     )
